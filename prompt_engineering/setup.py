a81a81a0c1c8cd66f42cc1dfdf5e8bb905fbc1b0
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-06-28 12:38:40 +0530

    install assets before starting task

--- prompt_engineering/setup.py
+++ prompt_engineering/setup.py
@@ -2,14 +2,12 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset
 
 dataset = load_dataset("ought/raft", "tai_safety_research")
-dataset.save_to_disk("resources/raft/tai_safety_research")
+dataset.save_to_disk("assets/raft/tai_safety_research")
 
 model_name = "bigscience/bloom-560m"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer.save_pretrained("resources/bloom-560m/tokenizer")
+tokenizer.save_pretrained("assets/bloom-560m/tokenizer")
 
 model = AutoModelForCausalLM.from_pretrained(model_name)
-model.save_pretrained("resources/bloom-560m/model")
-
-
+model.save_pretrained("assets/bloom-560m/model")
