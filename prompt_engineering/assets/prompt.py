c61c0ff7e77bf59fd3495ac4e1d9a9b41a8a2095
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-06-28 18:11:22 +0530

    simplify prompt

--- prompt_engineering/assets/prompt.py
+++ prompt_engineering/assets/prompt.py
@@ -1,10 +1,7 @@
 import evaluate
 
 def get_prompt(abstract_note: str, class_names: list[str]) -> str:
-     return f"""Classifty the following abstract as either '{class_names[0]}' or '{class_names[1]}'. 
-            \n{abstract_note}\n
-            Classification: 
-            """
+     return f"""{abstract_note}\n\nClassification: """
 
 if __name__ == '__main__':
     accuracy = evaluate.calculate_accuracy(get_prompt)
