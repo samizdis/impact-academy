5536588f969ce13f301cd41b12a11d2e5f3627c6
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-07-01 11:17:37 +0530

    simplify initial prompt file

--- prompt_engineering/assets/prompt.py
+++ prompt_engineering/assets/prompt.py
@@ -1,8 +1,2 @@
-import evaluate
-
 def get_prompt(abstract_note: str, class_names: list[str]) -> str:
      return f"""{abstract_note}\n\nClassification: """
-
-if __name__ == '__main__':
-    accuracy = evaluate.calculate_accuracy(get_prompt)
-    print(f"{accuracy:.2f}")
