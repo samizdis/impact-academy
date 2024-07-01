a1a8cbf9173a603276eae4e7148c1a22a8351b3c
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-07-01 12:22:33 +0530

    improve sample soln

--- prompt_engineering/sample_solution.py
+++ prompt_engineering/sample_solution.py
@@ -1,2 +1,2 @@
 def get_prompt(abstract_note: str, class_names: list[str]) -> str:
-     return f"""I will classify the following abstract into one of {", ".join(class_names)}. I will output just one of these classifications and nothing else. \n\nAbstract: {abstract_note}\n\nClassification: """
+     return f"""TAI stands for Transformative AI. I will classify the following abstract as either {class_names[1]} or {class_names[0]}. \n\nAbstract: {abstract_note}\n\n I will now output just one of the class names and nothing else. Classification: """
