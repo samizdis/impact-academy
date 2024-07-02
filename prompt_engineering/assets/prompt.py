0850a3b8defdff82dc8cdfa1b0cb1be197f8c7b0
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-07-02 12:38:38 +0530

    include code to run evaluation

--- prompt_engineering/assets/prompt.py
+++ prompt_engineering/assets/prompt.py
@@ -1,2 +1,6 @@
 def get_prompt(abstract_note: str, class_names: list[str]) -> str:
      return f"""{abstract_note}\n\nClassification: """
+
+if __name__ == '__main__':
+     from evaluate import calculate_accuracy
+     print(calculate_accuracy(get_prompt))
