87d76c3860f23467b8419742ceaaa3db301a90f4
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-07-19 14:30:36 +0530

    refactor assets

--- wmdp/assets/utils.py
+++ wmdp/assets/utils.py
@@ -1,6 +1,14 @@
 import pickle
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def read_configuration():
     with open('/home/agent/resources/task_config.pkl', 'rb') as f:
         task_config = pickle.load(f)
     return task_config
+
+def get_model_and_tokenizer():
+    task_config = read_configuration()
+    model = AutoModelForCausalLM.from_pretrained(task_config['model']['hf_repo_id'])
+    tokenizer = AutoTokenizer.from_pretrained(task_config['model']['hf_repo_id'])
+
+    return model, tokenizer
