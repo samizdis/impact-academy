c6528ece825808a21cf03613ac84fb0a9e10531e
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-07-31 12:36:06 +0530

    create venv and consolidate shell commands

--- wmdp/wmdp.py
+++ wmdp/wmdp.py
@@ -74,36 +74,23 @@ class TaskFamily:
     @staticmethod
     def get_aux_vm_spec(t: Task) -> VMSpec | None:
         return VMSpec(
-            cpu_count_range=(4, 4),
+            cpu_count_range=(1, 2),
             cpu_architecture="x64",
-            ram_gib_range=(16, 16),
-            gpu_spec=GPUSpec(count_range=(1,1), model="a10"),
-            base_image_type="ubuntu-20.04-cuda",
+            ram_gib_range=(1, 4),
+            # gpu_spec=GPUSpec(count_range=(1,1), model="a10"),
+            base_image_type="debian-12",
             build_steps=[
                 {
                     "type": "shell",
                     "commands": [
-                        "sudo apt update -y"
+                        "sudo apt update -y",
+                        "sudo apt install -y python3-pip python3-venv",
+                        "python3 -m venv myenv",
+                        ". myenv/bin/activate",
+                        # "python3 -m pip install torch==2.3.0 transformers==4.41.1 datasets==2.19.1 lm-eval==0.4.3 sentencepiece==0.2.0",
+                        
                     ]
                 },
-                {
-                    "type": "shell",
-                    "commands": [
-                        "sudo apt install -y python3-pip python3-venv"
-                    ]
-                },
-                {
-                    "type": "shell",
-                    "commands": [
-                        "python3 -m venv myenv"
-                    ]
-                },
-                # {
-                #     "type": "shell",
-                #     "commands": [
-                #         "python3 -m pip install torch==2.3.0 transformers==4.41.1 datasets==2.19.1 lm-eval==0.4.3 sentencepiece==0.2.0"
-                #     ]
-                # },
                 {
                     "type": "file",
                     "source": "./assets",
