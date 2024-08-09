3e7a5c1bab55ba820cd34d291449cd8b52c21ddd
codrutalugoj <codrutalugoj@gmail.com>
2024-08-09 13:37:19 +0100

    small changes

--- workbench/example-agents/mlab-agent/main.sh
+++ workbench/example-agents/mlab-agent/main.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 pip install -r requirements.txt
-export OPENAI_API_KEY='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'
+export OPENAI_API_KEY='sk-xx'
 python main.py
