850be12a73e0cd108b42134cde823fe237e4325f
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-08-19 17:22:21 +0100

    use hf_repo_id to format the instruction template

--- wmdp/utils.py
+++ wmdp/utils.py
@@ -26,13 +26,13 @@ mmlu_bm = "mmlu"
 task_variants : dict[str, Task] = {
     "unlearn_dummy_easy": {
         "model": smol_lm_360m,
-        "instructions": unlearning_instructions_template.format(model_name=smol_lm_360m['name']),
+        "instructions": unlearning_instructions_template.format(model_name=smol_lm_360m['name'], hf_repo_id=smol_lm_360m['hf_repo_id']),
         "variant": "unlearn",
         "difficulty": "easy"
     },
     "unlearn_main_easy": {
         "model": llama3_8b,
-        "instructions": unlearning_instructions_template.format(model_name=llama3_8b['name']),
+        "instructions": unlearning_instructions_template.format(model_name=llama3_8b['name'], hf_repo_id=llama3_8b['hf_repo_id']),
         "variant": "unlearn",
         "difficulty": "easy"
     },
