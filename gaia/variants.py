968638c7baaa0678b87bbd4da7b9d308a29948b1
Sai Sasank <sai.sasank.yadati@gmail.com>
2024-09-30 21:58:34 +0530

    add answer and file names to variants

---
+++ gaia/variants.py
@@ -0,0 +1,69 @@
+task_variants = {
+    "main": {
+        "instructions": "",
+        "dev_task_ids": {
+            "Game Show Puzzle": {
+                "task_id": "ec09fa32-d03f-4bf8-84b0-1f16922c3ae4",
+                "file_name": "",
+                "true_answer": "3",
+            },
+            "Secret Santa": {
+                "task_id": "cffe0e32-c9a6-4c52-9877-78ceb4aaa9fb",
+                "file_name": "cffe0e32-c9a6-4c52-9877-78ceb4aaa9fb.docx",
+                "true_answer": "Fred",
+            },
+            "Bags of Potatoes": {
+                "task_id": "dc28cf18-6431-458b-83ef-64b3ce566c10",
+                "file_name": "",
+                "true_answer": "2",
+            },
+            "Spreadsheet Sales": {
+                "task_id": "7cc4acfa-63fd-4acc-a1a1-e8e529e0a97f",
+                "file_name": "7cc4acfa-63fd-4acc-a1a1-e8e529e0a97f.xlsx",
+                "true_answer": "Wharvton",
+            },
+            "Seagull": {
+                "task_id": "50ad0280-0819-4bd9-b275-5de32d3b5bcb",
+                "file_name": "",
+                "true_answer": "The seagull glided peacefully to my chair.",
+            },
+            "Guava vs Pineapple": {
+                "task_id": "4b650a35-8529-4695-89ed-8dc7a500a498",
+                "file_name": "",
+                "true_answer": "Guava",
+            },
+        },
+        "test_task_ids": {
+            "Gold Coin Adventure": {
+                "task_id": "6af95c8f-8cbf-4c12-b02c-f9a23cc1ecb9",
+                "file_name": "",
+                "true_answer": "?",
+            },
+            "Excel Locomotive": {
+                "task_id": "cfd773c8-371f-425c-b081-f254f96c0530",
+                "file_name": "cfd773c8-371f-425c-b081-f254f96c0530.xlsx",
+                "true_answer": "?",
+            },
+            "Precipitation CSV": {
+                "task_id": "7805912b-c8da-4134-9b54-b590f884352a",
+                "file_name": "7805912b-c8da-4134-9b54-b590f884352a.csv",
+                "true_answer": "?",
+            },
+            "Find the Line": {
+                "task_id": "f1ba834a-3bcb-4e55-836c-06cc1e2ccb9f",
+                "file_name": "f1ba834a-3bcb-4e55-836c-06cc1e2ccb9f.txt",
+                "true_answer": "?",
+            },
+            "Integer Fun": {
+                "task_id": "314b3c26-3e8d-4f8c-a9aa-c77275072e0b",
+                "file_name": "",
+                "true_answer": "?",
+            },
+            "Twinkle Twinkle": {
+                "task_id": "68ccf11a-bcd3-41e5-a5ee-3e29253449e9",
+                "file_name": "68ccf11a-bcd3-41e5-a5ee-3e29253449e9.docx",
+                "true_answer": "?",
+            },
+        },
+    }
+}
