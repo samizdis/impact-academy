e18d37efea489045dbb250d13789b34460c3e7e7
codrutalugoj <codrutalugoj@gmail.com>
2024-10-18 16:09:54 +0100

    task setup with agent attempting the task

--- vul_id_expl/vul_id_expl.py
+++ vul_id_expl/vul_id_expl.py
@@ -33,12 +33,16 @@ class TaskFamily:
     @staticmethod
     def install() -> None:
         run_command(f"pip install openai python-dotenv")
+        run_command("cp ./.env /home/agent/.env")
 
         # CSE2 Vulnerability ID and exploitation-specific setup
-        run_command("git clone https://github.com/meta-llama/PurpleLlama.git")
-        run_command("pip3 install -r /root/PurpleLlama/CybersecurityBenchmarks/requirements.txt")
+        run_command("git clone https://github.com/meta-llama/PurpleLlama.git /home/agent/PurpleLlama")
+        run_command("pip3 install -r /home/agent/PurpleLlama/CybersecurityBenchmarks/requirements.txt")
+        run_command("export DATASETS=$/home/agent/PurpleLlama/CybersecurityBenchmarks/datasets")
+        run_command(f"export OPENAI_API_KEY={os.environ['OPENAI_API_KEY']}")
 
-
+        logging.info(f"{os.environ['OPENAI_API_KEY']}")
+        run_command(f"echo -e \"export OPENAI_API_KEY=\"{os.environ['OPENAI_API_KEY']}\"\" >> ~/.bashrc")
 
     @staticmethod
     def get_tasks() -> dict[str, Task]:
@@ -66,29 +70,63 @@ class TaskFamily:
 
     @staticmethod
     def run_evaluation(t: Task)-> None:
-        from resources.canary_exploit import canary_exploit_benchmark
-        DATASET = "/root/canary_exploit"
-        command = (
-                f"python3 canary_exploit.run "
-                f"benchmark={DATASET} "
-                f"prompt-path={DATASET}.json "
-                f"response-path={DATASET}/canary_exploit_responses.json "
-                f"judge-response-path={DATASET}/canary_exploit_judge_responses.json "
-                f"stat-path={DATASET}/canary_exploit_stat.json "
-                f"llm-under-test=OPENAI::gpt-4o::{os.environ['OPENAI_API_KEY']}"
-                )
+        logging.info("CURRENT WD", os.getcwd())
 
-        subprocess.call(command, shell=True)
+        DATA = "/home/agent/PurpleLlama/CybersecurityBenchmarks/datasets"
+
+        command = f"python3 -m PurpleLlama.CybersecurityBenchmarks.benchmark.run " \
+          f"--benchmark=\"canary-exploit\" " \
+          f"--prompt-path=\"/home/agent/resources/canary_exploit/training.json\" " \
+          f"--response-path=\"{DATA}/canary_exploit/canary_exploit_responses.json\" " \
+          f"--judge-response-path=\"{DATA}/canary_exploit/canary_exploit_judge_responses.json\" " \
+          f"--stat-path=\"{DATA}/canary_exploit/canary_exploit_stat.json\" " \
+          f"--llm-under-test=OPENAI::gpt-4o::{os.environ['OPENAI_API_KEY']}"
+
+        #subprocess.call(command, shell=True)
         
 
     @staticmethod
     def start(t: Task) -> None:
 
-        with open("resources/training_prompts.json", "r") as f:
-            prompts = json.load(f)
-            
-        TaskFamily.run_evaluation()        
+        run_command("cp -r /root/resources /home/agent/resources")
+        run_command("chown -R agent:agent /home/agent/resources")
+        run_command("chmod +x /home/agent/resources/canary_exploit/eval.sh")
 
+        import os
+        import tempfile
+        from pathlib import Path
+        import importlib.resources
+        import shutil
+
+        def _get_semgrep_core_path() -> Path:
+            semgrep_core_name = "semgrep-core"
+            try:
+                with importlib.resources.path("semgrep.bin", semgrep_core_name) as path:
+                    if path.is_file():
+                        return path
+            except (FileNotFoundError, ModuleNotFoundError):
+                pass
+
+            path = shutil.which(semgrep_core_name)
+            if path is not None:
+                return Path(path)
+
+            raise Exception(
+                f"Failed to find {semgrep_core_name} in PATH or in the semgrep package."
+            )
+        
+        def make_semgrep_binary_path():
+            source = "/usr/local/lib/python3.11/site-packages/semgrep/bin/semgrep-core"
+            # Use a temporary directory that the user has write access to
+            temp_dir = tempfile.gettempdir()
+            destination = os.path.join(temp_dir, "osemgrep")
+            os.symlink(source, destination)
+            return Path(destination)
+
+        source = _get_semgrep_core_path()
+        destination = Path(str(source).replace("semgrep-core", "osemgrep"))
+        os.symlink(source, destination)
+        
 
 
     @staticmethod
